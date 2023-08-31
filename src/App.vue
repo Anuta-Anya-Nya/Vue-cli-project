e56e8bde1221f84acdfffbef03db29242b603eda
Aeshe <syslichek@mail.ru>
2023-08-31 14:45:03 +0300

    add header component

--- src/App.vue
+++ src/App.vue
@@ -1,28 +1,43 @@
 <template>
   <div id="app">
-    <img alt="Vue logo" src="./assets/logo.png">
-    <HelloWorld msg="Welcome to Your Vue.js App"/>
+    <HeaderBlock />
   </div>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+import HeaderBlock from './blocks/HeaderBlock.vue'
+
 
 export default {
   name: 'App',
   components: {
-    HelloWorld
+    HeaderBlock
   }
 }
 </script>
 
 <style lang="scss">
 #app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
+  margin-top: 53px;
 }
+
+  * {
+        margin: 0;
+        padding: 0;
+        box-sizing: border-box;
+    }
+
+    ul {
+        list-style: none;
+    }
+
+    a {
+        text-decoration: none;
+    }
+
+    .container {
+        max-width: 1200px;
+        margin: 0 auto;
+    }
+
 </style>
---
+++ src/blocks/BannerMainPage.vue
@@ -0,0 +1,55 @@
+<template>
+    <section class="banner container">
+        <div class="banner__box">
+          <div class="banner__content">
+            <h2 class="banner__title">Let Your Home Be Unique</h2>
+            <p class="banner__text">
+              There are many variations of the passages of lorem Ipsum
+              fromavailable, majority.
+            </p>
+            <button class="button">
+              Get Started
+              <svg
+                width="19"
+                height="17"
+                viewBox="0 0 19 17"
+                fill="none"
+                xmlns="http://www.w3.org/2000/svg"
+              >
+                <path
+                  d="M2 8.43542L15.7232 8.29857M10.6182 1.91138L17.1412 8.43436L10.4868 15.0887"
+                  stroke="#CDA274"
+                  stroke-width="2"
+                  stroke-linecap="square"
+                  stroke-linejoin="round"
+                />
+              </svg>
+            </button>
+          </div>
+        </div>
+      </section>
+</template>
+
+<script>
+export default {
+    name: 'BannerMainPage',
+
+    data() {
+        return {
+            
+        };
+    },
+
+    mounted() {
+        
+    },
+
+    methods: {
+        
+    },
+};
+</script>
+
+<style lang="scss" scoped>
+
+</style>
