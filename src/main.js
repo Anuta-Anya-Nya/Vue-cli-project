3e6aee2fa191b4adcfcba761dc524c89b638f851
Aeshe <syslichek@mail.ru>
2023-09-19 15:31:49 +0300

    add router

--- src/main.js
+++ src/main.js
@@ -1,14 +1,14 @@
 import Vue from 'vue'
 import App from './App.vue'
-import BootstrapVue from 'bootstrap-vue'
 import store from './store'
-import 'bootstrap'
-import 'bootstrap/dist/css/bootstrap.min.css'
+import router from './router'
+
 
 Vue.config.productionTip = false
-Vue.use(BootstrapVue)
+
 
 new Vue({
+  router,
   store,
   render: h => h(App),
 }).$mount('#app')
